e1b5f37ee3e997cf90a3d435ed97838430db254d
Ruslan Khitseyeu <ruslankhitseyeu@gmail.com>
2022-10-22 19:19:16 +0200

    first commit

---
+++ 124.js
@@ -0,0 +1,8 @@
+function Ruslan()
+const Ruslan = new Ruslan;
+if(Ruslan.constructor.name === "Ruslan")
+{
+    console.log ("Ruslan hello");
+}else{
+    console.log("no such name");
+}
---
+++ 125.js
@@ -0,0 +1,11 @@
+const warehouse = {
+    apple: 10,
+    pear: 10
+};
+const getNumFruit = fruit => {
+    return waterhouse [fruit];
+};
+const numApples = getNumFruit("apple");
+const numPears = getNumFruit("pear");
+console.log(numApples);
+console.log(numPears);
