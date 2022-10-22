cb903bccc14b365d29ecc45432f69bcb21b97945
Ruslan Khitseyeu <ruslankhitseyeu@gmail.com>
2022-10-22 18:24:42 +0200

    Data

---
+++ data.js
@@ -0,0 +1,17 @@
+//Perform addition of various types (string + boolean, string + number, number + boolean)
+true + 1 //Boolean + Number
+'foo' + false //String + Boolean
+5 + 'foo' //Number + String
+//Perform multiplication of various types (string * boolean, string * number, number * boolean)
+'foo' * 2//Number + String
+'foo' * false //string * boolean
+true * 1//number * boolean
+//Divide different types (string / boolean, string / number, number / Boolean)
+true / 1//number / Boolean
+'foo' / false//string / boolean
+'foo' / 2//Number / String
+//Perform explicit conversion (number, string, boolean)
+alert( Boolean(1) ); // boolean
+alert(typeof num);//number
+let str = "123";
+alert(typeof str); // string
